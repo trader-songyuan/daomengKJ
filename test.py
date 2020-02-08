d6acf80e7b7f4a482b8765dc54d1b7e8afede40f
Vinting <vintingb@gmail.com>
2020-02-08 20:46:49 +0800

    Update test.py

--- test.py
+++ test.py
@@ -30,29 +30,14 @@ class Post:
         url = "https://appdmkj.5idream.net/v2/activity/activities"
 
         res = requests.post(url, headers=self.headers, data=data).json()
-        # print(res['data']['list'][1])
+        # print(res)
         names = jsonpath.jsonpath(res, '$..name')
         ids = jsonpath.jsonpath(res, '$..aid')
         statusTexts = jsonpath.jsonpath(res, '$..statusText')
         self.names = names
         self.ids = ids
         self.statusTexts = statusTexts
-        # for name, id, statusText in zip(names, ids, statusTexts):
-        #     print(name, id, statusText)
-        # b = {}
-        # for name, id, statusText in zip(names, ids, statusTexts):
-        #     if '补发' in name:
-        #         None
-        #     elif '已结束' in statusText:
-        #         print(name + '已经结束')
-        #     elif '团日' in name:
-        #         None
-        #     elif '青年大学习' in name:
-        #         None
-        #     else:
-        #         b[name] = id
-        # print(b)
-        # return b
+        return res
 
     def get_info(self, id, token, uid):
         signtoken = get_signtoken(
@@ -63,25 +48,10 @@ class Post:
             jiami(str1))
         url = "https://appdmkj.5idream.net/v2/activity/detail"
         res = requests.post(url, headers=self.headers, data=data)
-
-        if res.json()['code'] == '100':
-            return res
-        else:
-            return None
-
-    def get_join(self, token, uid):
-        signtoken = get_signtoken(
-            '{"catalogId":"","catalogId2":"","endTime":"","joinEndTime":"","joinFlag":"1","joinStartTime":"","keyword":"","level":"","page":"1","sort":"","specialFlag":"","startTime":"","status":"","token":"' + token + '","uid":' + uid + ',"version":"4.2.6"}')
-        str1 = '{"catalogId":"","catalogId2":"","endTime":"","joinEndTime":"","joinFlag":"1","joinStartTime":"","keyword":"","level":"","page":"1","signToken":"' + signtoken + '","sort":"","specialFlag":"","startTime":"","status":"","token":"' + token + '","uid":' + uid + ',"version":"4.2.6"}'
-        data = "dataKey=t%2BZ88oeo2xscPIEBzd1JWLr%2Faae06xI9WOwwXOVRupB%2BsAsl1nj2HDpZPc3ygHRlgm0glZajSvF7FsxbGiBe%2FcykCvyhloLZfYPGGLrCZV6ZBVDBHgwg6%2Fkq87A6A%2Bp%2BmTeUyp3eZz4voIGytVkwmlofr0Jn5bgBOitzBJtnq0I%3D&data={}".format(
-            jiami(str1))
-        url = 'https://appdmkj.5idream.net/v2/activity/activities'
-        res = requests.post(url, headers=self.headers, data=data)
-
-        if res.json()['code'] == '100':
+        try:
             return res.json()
-        else:
-            return None
+        except:
+            return False
 
     def get_can_join(self, token, uid):
         signtoken = get_signtoken(
@@ -104,20 +74,53 @@ class Post:
         else:
             return '没有活动可以参加'
 
+    def join(self, id, token, uid):
+        signtoken = get_signtoken(
+            '{"activityId":"' + id + '","data":"[]","remark":"","token":"' + token + '","uid":"' + uid + '","version":"4.2.6"}')
+        str1 = '{"activityId":"' + id + '","data":"[]","remark":"","signToken":"' + signtoken + '","token":"' + token + '","uid":"' + uid + '","version":"4.2.6"}'
+        data = "dataKey=t%2BZ88oeo2xscPIEBzd1JWLr%2Faae06xI9WOwwXOVRupB%2BsAsl1nj2HDpZPc3ygHRlgm0glZajSvF7FsxbGiBe%2FcykCvyhloLZfYPGGLrCZV6ZBVDBHgwg6%2Fkq87A6A%2Bp%2BmTeUyp3eZz4voIGytVkwmlofr0Jn5bgBOitzBJtnq0I%3D&data={}".format(
+            jiami(str1))
+        url = 'https://appdmkj.5idream.net/v2/signup/submit'
+        res = requests.post(url, headers=self.headers, data=data)
+        try:
+            return res.json()
+        except:
+            return False
 
-if __name__ == '__main__':
+
+
+def test_token():
     a = Post()
     try:
         with open('a.ini', 'r', encoding='utf-8') as f:
             token = f.readline().rstrip()
             name = f.readline().rstrip()
             uid = f.readline().rstrip()
-            # a.get_ids(token, uid)
-            # a.get_can_join(token, uid)
-            # print(a.get_info('1774799', token, uid).text)
+            res = a.get_ids(token, uid)
+            if res['code'] == '100':
+                print('成功')
+                return True
+            else:
+                print('失败')
+                return False
     except:
         None
-        # print("token失效,重新获取")
 
-        # a.get_ids(token)
-        # a.get_ids(a.ids[1], token)
+
+# if __name__ == '__main__':
+#     a = Post()
+#     try:
+#         with open('a.ini', 'r', encoding='utf-8') as f:
+#             token = f.readline().rstrip()
+#             name = f.readline().rstrip()
+#             uid = f.readline().rstrip()
+#             a.join('1774799', token, uid)
+#             # a.get_ids('token', 'uid')
+#             # a.get_can_join(token, uid)
+#             # print(a.get_info('1774799', token, uid).text)
+#     except:
+#         None
+#         # print("token失效,重新获取")
+#
+#         # a.get_ids(token)
+#         # a.get_ids(a.ids[1], token)
