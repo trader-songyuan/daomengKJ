3633c572c5337dfff83d600395c25807ec9bd59b
Vinting <vintingb@gmail.com>
2020-02-08 20:46:38 +0800

    Update daomeng.py

--- daomeng.py
+++ daomeng.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import tkinter.messagebox as messagebox
 from tkinter import StringVar
 from jiami import *
-from test import Post
+from test import Post, test_token
+import os
 
 app = tk.Tk()
 app.title('到梦空间')
@@ -29,33 +30,34 @@ id2 = tk.Entry(app, font=('Arial', 14), width=7)
 id2.pack()
 id2.place(x=620, y=35)
 
+
 class Main:
-    try:
+
+    def read(self):
         with open('a.ini', 'r', encoding='utf-8') as f:
             self.token = f.readline().rstrip()
             self.name = f.readline().rstrip()
             self.uid = f.readline().rstrip()
-    except:
-        None
+
     def login(self):
         acc = entry_usr_name.get()
         pwd = entry_usr_pwd.get()
-        try:
-            with open('a.ini', 'r', encoding='utf-8') as f:
-                self.token = f.readline().rstrip()
-                self.name = f.readline().rstrip()
-                self.uid = f.readline().rstrip()
-        except:
-            print('token失效重新获取')
-            get_token(acc, pwd)
-            with open('a.ini', 'r', encoding='utf-8') as f:
-                self.token = f.readline().rstrip()
-                self.name = f.readline().rstrip()
-                self.uid = f.readline().rstrip()
 
+        if os.path.exists('a.ini'):
+            if test_token():
+                return True
+            else:
+                messagebox.showwarning(title='出错了', message='登录失效，请重新登录')
+                os.remove('a.ini')
+                return False
+        else:
+            if get_token(acc, pwd):
+                return True
+            else:
+                messagebox.showwarning(title='出错了', message='请检查账号密码')
+                return False
 
     def get_id(self):
-
         a = Post()
         a.get_ids(self.token, self.uid)
         messagebox.showinfo('欢迎您', self.name)
@@ -70,35 +72,63 @@ class Main:
     def chiken(self):
         id = id1.get()
         a = Post()
-        res = a.get_info(id, self.token, self.uid).json()
-        # activityName = res['data']['activityName']   #活动名称
-        # address = res['data']['address']   #活动地址
-        # joindate = res['data']['joindate']  #报名时间
-        # startdate = res['data']['startdate']  #活动时间
-        # name = res['data']['specialList'][0]['name']  #积分类型
-        # unitcount = res['data']['specialList'][0]['unitcount']  #积分数量
+        res = a.get_info(id, self.token, self.uid)
+        if res:
+            app1 = tk.Toplevel(app)
+            app1.geometry('643x360')
+            app1.title('详细信息')
+            tk.Label(app1, text=res['data']['activityName'], font=('Arial', 14)).place(x=100, y=0)
+            tk.Label(app1, text='活动名称', font=('Arial', 14)).place(x=0, y=0)
+            tk.Label(app1, text=res['data']['address'], font=('Arial', 14)).place(x=100, y=25)
+            tk.Label(app1, text='活动地址', font=('Arial', 14)).place(x=0, y=25)
+            tk.Label(app1, text=res['data']['joindate'], font=('Arial', 14)).place(x=100, y=50)
+            tk.Label(app1, text='报名时间', font=('Arial', 14)).place(x=0, y=50)
+            tk.Label(app1, text=res['data']['startdate'], font=('Arial', 14)).place(x=100, y=75)
+            tk.Label(app1, text='活动时间', font=('Arial', 14)).place(x=0, y=75)
+            tk.Label(app1, text=res['data']['specialList'][0]['name'], font=('Arial', 14)).place(x=100, y=100)
+            tk.Label(app1, text='积分类型', font=('Arial', 14)).place(x=0, y=100)
+            tk.Label(app1, text=res['data']['specialList'][0]['unitcount'], font=('Arial', 14)).place(x=100, y=125)
+            tk.Label(app1, text='积分数量', font=('Arial', 14)).place(x=0, y=125)
+        else:
+            messagebox.showwarning(title='出错了', message='查询失败，请检查id')
 
-        app1 = tk.Toplevel(app)
-        app1.geometry('643x360')
-        app1.title(res['data']['activityName'])
-
-
-
+    def enter(self):
+        id = id2.get()
+        a = Post()
+        res = a.join(id, self.token, self.uid)
+        if res:
+            messagebox.showinfo(title='报名详情', message=res['msg'])
+        else:
+            messagebox.showwarning(title='出错了', message='查询失败，请检查id')
 
 
 def login():
     main = Main()
-    main.login()
-    main.get_id()
-
+    if main.login():
+        main.read()
+        main.get_id()
+    else:
+        None
 
 
 def chiken():
     main = Main()
-    main.login()
-    main.chiken()
+    if main.login():
+        main.read()
+        main.chiken()
+    else:
+        pass
 
-b = tk.Button(app, text='登录', font=('Arial', 12), width=10, height=1, command=login)
+def join():
+    main = Main()
+    if main.login():
+        main.read()
+        main.enter()
+    else:
+        pass
+
+
+b = tk.Button(app, text='登录查询', font=('Arial', 12), width=10, height=1, command=login)
 b.pack()
 b.place(x=380, y=12.5)
 
@@ -106,9 +136,8 @@ b1 = tk.Button(app, text='活动信息查询', font=('Arial', 12), width=10, hei
 b1.pack()
 b1.place(x=720, y=0)
 
-b1 = tk.Button(app, text='报名活动', font=('Arial', 12), width=10, height=1, command=login)
+b1 = tk.Button(app, text='报名活动', font=('Arial', 12), width=10, height=1, command=join)
 b1.pack()
 b1.place(x=720, y=32)
-
 
 app.mainloop()
